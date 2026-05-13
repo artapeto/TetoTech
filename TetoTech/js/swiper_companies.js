dd06c59eedb1246304fa9e825e1eea8ce99176bf
artapeto <petronevich2@gmail.com>
2026-05-13 22:27:10 +0300

    было сделано 2 слайдера с добавлением в него контента

---
+++ TetoTech/js/swiper_companies.js
@@ -0,0 +1,22 @@
+const companiesSwiper = new Swiper(".swiper__companies", {
+
+    loop: true,
+
+    slidesPerView: "auto",
+
+    spaceBetween: 30,
+
+    speed: 5000,
+
+    allowTouchMove: false,
+
+    autoplay: {
+        delay: 0,
+        disableOnInteraction: false,
+    },
+
+    freeMode: true,
+
+    freeModeMomentum: false,
+
+});
---
+++ TetoTech/js/swiper_popular.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.swiper__popular', {
+   slidesPerView: 5,
+  spaceBetween: 30,
+
+    navigation: {
+    nextEl: '.btns__right',
+    prevEl: '.btns__left',
+  },
+
+});
