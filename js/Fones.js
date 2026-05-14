c3fbb492f5842e6887ee0cee2c4f0a330f5ad4e7
Arta <petreonevich2@gmail.com>
2026-05-14 22:27:22 +0300

    был сделан сайт каталого

--- js/Fones.js
+++ js/Fones.js
@@ -1,10 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
   const totalFones = 9;
 
-  document.querySelectorAll('.catalog_card').forEach(card => {
+  document.querySelectorAll('.catalog__card').forEach(card => {
     const random = Math.floor(Math.random() * totalFones) + 1;
     card.style.backgroundImage = `url(../images/catalog/fones/fon_${random}.png)`;
-    card.style.backgroundSize = 'cover';
-    card.style.backgroundPosition = 'center';
   });
 });
